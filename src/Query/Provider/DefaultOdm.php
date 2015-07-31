1fc6b2a776763dd2f7aecb47cdf24e7edd4ca9ab
Lennart Weijl <lennart@weijl.org>
2015-07-31 14:19:26 +0200

    Gets the filter ODM manager instead of the ORM manager.

--- src/Query/Provider/DefaultOdm.php
+++ src/Query/Provider/DefaultOdm.php
@@ -79,7 +79,8 @@ class DefaultOdm implements QueryProviderInterface, ServiceLocatorAwareInterface
 
         if (isset($request[$this->getFilterKey()])) {
             $metadata = $this->getObjectManager()->getMetadataFactory()->getAllMetadata();
-            $filterManager = $this->getServiceLocator()->get('ZfDoctrineQueryBuilderFilterManagerOrm');
+            $filterManager = $this->getServiceLocator()
+                ->getServiceLocator()->get('ZfDoctrineQueryBuilderFilterManagerOdm');
             $filterManager->filter(
                 $queryBuilder,
                 $metadata[0],
@@ -89,7 +90,8 @@ class DefaultOdm implements QueryProviderInterface, ServiceLocatorAwareInterface
 
         if (isset($request[$this->getOrderByKey()])) {
             $metadata = $this->getObjectManager()->getMetadataFactory()->getAllMetadata();
-            $orderByManager = $this->getServiceLocator()->get('ZfDoctrineQueryBuilderOrderByManagerOrm');
+            $orderByManager = $this->getServiceLocator()
+                ->getServiceLocator()->get('ZfDoctrineQueryBuilderOrderByManagerOdm');
             $orderByManager->orderBy(
                 $queryBuilder,
                 $metadata[0],
