9a12996f51a356aed5429437bbd426ebf9508ae7
Tom Anderson <tom@lodgable.com>
2015-09-18 15:45:27 -0600

    Renamed config to module.config.php

--- Module.php
+++ Module.php
@@ -24,7 +24,7 @@ class Module implements DependencyIndicatorInterface
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/server.config.php';
+        return include __DIR__ . '/config/module.config.php';
     }
 
     public function init(ModuleManager $moduleManager)
