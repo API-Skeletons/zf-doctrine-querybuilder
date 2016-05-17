5bcba6a5c1620b59de49b94a1841069bfd6e38e5
Silvério Miranda <silveriomiranda@live.com>
2016-05-17 04:11:03 -0300

    Add files via upload

---
+++ src/Filter/ORM/MatchAgainst.php
@@ -0,0 +1,60 @@
+<?php
+namespace ZF\Doctrine\QueryBuilder\Filter\ORM;
+
+class MatchAgainst extends AbstractFilter
+{
+
+    public function filter($queryBuilder, $metadata, $option)
+    {
+        if (isset($option['where'])) {
+            if ($option['where'] === 'and') {
+                $queryType = 'andWhere';
+            } elseif ($option['where'] === 'or') {
+                $queryType = 'orWhere';
+            }
+        }
+        
+        if (! isset($queryType)) {
+            $queryType = 'andWhere';
+        }
+        
+        if (! isset($option['alias'])) {
+            $option['alias'] = 'row';
+        }
+        
+        $format = isset($option['format']) ? $option['format'] : null;
+        
+        $value = $this->typeCastField($metadata, $option['field'], $option['value'], $format);
+        
+        // $parameter = uniqid('a');
+        
+        if (! is_array($option['field'])) {
+            if (strpos($option['field'], ',') !== false) {
+                $option['field'] = explode(',', $option['field']);
+            }
+        }
+        
+        if (is_array($option['field'])) {
+            $fields = array();
+            foreach ($option['field'] as $field) {
+                $field = utf8_encode($field);
+                $fields[] = "{$option['alias']}.{$field}";
+            }
+            $option['field'] = implode(',', $fields);
+        } else {
+            $option['field'] = "{$option['alias']}.{$option['field']}";
+        }
+        
+        $queryBuilder->addSelect("MATCH ({$option['field']}) AGAINST (:searchterm) match_score")
+            ->add('where', "MATCH ({$option['field']}) AGAINST (:searchterm) > 0.8")
+            ->setParameter('searchterm', $option['value'])
+            ->orderBy('match_score', 'desc');
+        
+        // $queryBuilder->$queryType(
+        // $queryBuilder
+        // ->expr()
+        // ->gt($option['alias'] . '.' . $option['field'], ':' . $parameter)
+        // );
+        // $queryBuilder->setParameter($parameter, $value);
+    }
+}
