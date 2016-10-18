6ecced5c4cb05765c5ed0c65bddd5e2e189a5aa9
webimpress <contact@webimpress.com>
2016-10-18 23:19:34 +0100

    Removed duplicated condition

--- src/OrderBy/Service/ORMOrderByManager.php
+++ src/OrderBy/Service/ORMOrderByManager.php
@@ -43,14 +43,12 @@ class ORMOrderByManager extends AbstractPluginManager
     public function validate($instance)
     {
         if (! $instance instanceof $this->instanceOf) {
-            if (! $instance instanceof $this->instanceOf) {
-                throw new Exception\InvalidServiceException(sprintf(
-                    '%s can only create instances of %s; %s is invalid',
-                    get_class($this),
-                    $this->instanceOf,
-                    is_object($instance) ? get_class($instance) : gettype($instance)
-                ));
-            }
+            throw new Exception\InvalidServiceException(sprintf(
+                '%s can only create instances of %s; %s is invalid',
+                get_class($this),
+                $this->instanceOf,
+                is_object($instance) ? get_class($instance) : gettype($instance)
+            ));
         }
     }
 
