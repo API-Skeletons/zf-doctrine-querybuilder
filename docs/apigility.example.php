e49949e19638cec84aa66b55cf6c1ba285ffcdc8
webimpress <contact@webimpress.com>
2016-11-02 00:02:48 +0000

    Updated usage example

--- docs/apigility.example.php
+++ docs/apigility.example.php
@@ -15,9 +15,9 @@
 
 namespace ApplicationApi;
 
-use ZF\Apigility\Provider\ApigilityProviderInterface;
-use ZF\Apigility\Doctrine\Server\Event\DoctrineResourceEvent;
 use Zend\Mvc\MvcEvent;
+use ZF\Apigility\Doctrine\Server\Event\DoctrineResourceEvent;
+use ZF\Apigility\Provider\ApigilityProviderInterface;
 
 class Module implements ApigilityProviderInterface
 {
@@ -27,33 +27,34 @@ class Module implements ApigilityProviderInterface
         $services = $application->getServiceManager();
         $sharedEvents = $services->get('SharedEventManager');
 
-        $sharedEvents->attach('ZF\Apigility\Doctrine\DoctrineResource',
-            DoctrineResourceEvent::EVENT_FETCH_ALL_PRE, function ($e) use ($application)
-            {
-                $objectManager = $application->getServiceManager()->get('doctrine.entitymanager.orm_default');
+        $sharedEvents->attach(
+            'ZF\Apigility\Doctrine\DoctrineResource',
+            DoctrineResourceEvent::EVENT_FETCH_ALL_PRE,
+            function (DoctrineResourceEvent $e) use ($application) {
+                $services = $application->getServiceManager();
+                $objectManager = $services->get('doctrine.entitymanager.orm_default');
                 $metadata = $objectManager->getMetadataFactory()->getAllMetadata();
 
                 // Fetch filter criteria
                 $request = $application->getRequest()->getQuery()->toArray();
-                if (!isset($request['query'])) {
+                if (! isset($request['query'])) {
                     return;
                 }
 
                 // Allow multiple filter managers based on entity
-                $filterManagers = array();
-                $orderByManagers = array();
+                $filterManagers = [];
+                $orderByManagers = [];
                 switch ($e->getEntity()) {
                     // Don't filter album queries; for example
                     case 'Db\Entity\Album':
-                        return;
                         break;
                     // Do apply ZF Doctrine QueryBuilder
                     case 'Db\Entity\Artist':
-                        $filterManagers[] = $application->getServiceManager()->get('ZfDoctrineQueryBuilderFilterManagerOrm');
-                        $orderByManagers[] = $application->getServiceManager()->get('ZfDoctrineQueryBuilderOrderByManagerOrm');
+                        $filterManagers[] = $services->get('ZfDoctrineQueryBuilderFilterManagerOrm');
+                        $orderByManagers[] = $services->get('ZfDoctrineQueryBuilderOrderByManagerOrm');
                         break;
                     default:
-                        return;
+                        // Define here default behaviour
                         break;
                 }
 
