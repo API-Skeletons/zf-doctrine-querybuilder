f023e1dba3b0014e8b5011a610416960cabfef48
webimpress <contact@webimpress.com>
2016-11-14 09:56:00 +0000

    DefaultOdmFactory and DefaultOrmFactory could be used with context of AbstractPluginManager - with ZF2 and zf-apigility-doctrine these factories are used with ZF\Apigility\Doctrine\Server\Query\Provider\Service\QueryProviderManager

--- test/Query/Provider/DefaultOdmFactoryTest.php
+++ test/Query/Provider/DefaultOdmFactoryTest.php
@@ -7,6 +7,7 @@
 namespace ZFTest\Doctrine\QueryBuilder\Query\Provider;
 
 use PHPUnit_Framework_TestCase as TestCase;
+use Zend\ServiceManager\AbstractPluginManager;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use ZF\Doctrine\QueryBuilder\Query\Provider\DefaultOdm;
 use ZF\Doctrine\QueryBuilder\Query\Provider\DefaultOdmFactory;
@@ -19,6 +20,19 @@ class DefaultOdmFactoryTest extends TestCase
 
         $factory = new DefaultOdmFactory();
         $provider = $factory($serviceLocator);
+
+        $this->assertInstanceOf(DefaultOdm::class, $provider);
+        $this->assertAttributeSame($serviceLocator, 'serviceLocator', $provider);
+    }
+
+    public function testInvokableFactoryReturnsDefaultOdmQueryProviderWhenCreatedViaAbstractPluginManager()
+    {
+        $serviceLocator = $this->prophesize(ServiceLocatorInterface::class)->reveal();
+        $abstractPluginManager = $this->prophesize(AbstractPluginManager::class);
+        $abstractPluginManager->getServiceLocator()->willReturn($serviceLocator);
+
+        $factory = new DefaultOdmFactory();
+        $provider = $factory($abstractPluginManager->reveal());
 
         $this->assertInstanceOf(DefaultOdm::class, $provider);
         $this->assertAttributeSame($serviceLocator, 'serviceLocator', $provider);
--- test/Query/Provider/DefaultOrmFactoryTest.php
+++ test/Query/Provider/DefaultOrmFactoryTest.php
@@ -7,6 +7,7 @@
 namespace ZFTest\Doctrine\QueryBuilder\Query\Provider;
 
 use PHPUnit_Framework_TestCase as TestCase;
+use Zend\ServiceManager\AbstractPluginManager;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use ZF\Doctrine\QueryBuilder\Query\Provider\DefaultOrm;
 use ZF\Doctrine\QueryBuilder\Query\Provider\DefaultOrmFactory;
@@ -19,6 +20,19 @@ class DefaultOrmFactoryTest extends TestCase
 
         $factory = new DefaultOrmFactory();
         $provider = $factory($serviceLocator);
+
+        $this->assertInstanceOf(DefaultOrm::class, $provider);
+        $this->assertAttributeSame($serviceLocator, 'serviceLocator', $provider);
+    }
+
+    public function testInvokableFactoryReturnsDefaultOrmQueryProviderWhenCreatedViaAbstractPluginManager()
+    {
+        $serviceLocator = $this->prophesize(ServiceLocatorInterface::class)->reveal();
+        $abstractPluginManager = $this->prophesize(AbstractPluginManager::class);
+        $abstractPluginManager->getServiceLocator()->willReturn($serviceLocator);
+
+        $factory = new DefaultOrmFactory();
+        $provider = $factory($abstractPluginManager->reveal());
 
         $this->assertInstanceOf(DefaultOrm::class, $provider);
         $this->assertAttributeSame($serviceLocator, 'serviceLocator', $provider);
